28dbd08683bf15aaa17010c47bea8cff65aea23f
jordan-sisto <jordan.sisto@gmail.com>
2025-06-09 23:19:19 -0500

    Add files via upload

---
+++ trench_data.py
@@ -0,0 +1,323 @@
+# Auto-generated Trench Crusade customization data
+all_customizable_items = [
+  {
+    "name": "10",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "15",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "6",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "30",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "7",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "50",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "55",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "1",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "1",
+    "cost": NaN,
+    "type": "Melee Weapons"
+  },
+  {
+    "name": "2",
+    "cost": NaN,
+    "type": "Melee Weapons"
+  },
+  {
+    "name": "3",
+    "cost": NaN,
+    "type": "Melee Weapons"
+  },
+  {
+    "name": "4",
+    "cost": NaN,
+    "type": "Melee Weapons"
+  },
+  {
+    "name": "7",
+    "cost": NaN,
+    "type": "Melee Weapons"
+  },
+  {
+    "name": "10",
+    "cost": NaN,
+    "type": "Melee Weapons"
+  },
+  {
+    "name": "12",
+    "cost": NaN,
+    "type": "Melee Weapons"
+  },
+  {
+    "name": "23",
+    "cost": NaN,
+    "type": "Melee Weapons"
+  },
+  {
+    "name": "15",
+    "cost": NaN,
+    "type": "Melee Weapons"
+  },
+  {
+    "name": "40",
+    "cost": NaN,
+    "type": "Melee Weapons"
+  },
+  {
+    "name": "5",
+    "cost": NaN,
+    "type": "Melee Weapons"
+  },
+  {
+    "name": "15",
+    "cost": NaN,
+    "type": "Armor"
+  },
+  {
+    "name": "40",
+    "cost": NaN,
+    "type": "Armor"
+  },
+  {
+    "name": "10",
+    "cost": NaN,
+    "type": "Armor"
+  },
+  {
+    "name": "5",
+    "cost": NaN,
+    "type": "Armor"
+  },
+  {
+    "name": "3",
+    "cost": NaN,
+    "type": "Armor"
+  },
+  {
+    "name": "25",
+    "cost": NaN,
+    "type": "Armor"
+  },
+  {
+    "name": "30",
+    "cost": NaN,
+    "type": "Armor"
+  },
+  {
+    "name": "40",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "20",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "5",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "2",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "3",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "4",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "35",
+    "cost": NaN,
+    "type": "Melee Weapons"
+  },
+  {
+    "name": "7",
+    "cost": NaN,
+    "type": "Armor"
+  },
+  {
+    "name": "20",
+    "cost": NaN,
+    "type": "Armor"
+  },
+  {
+    "name": "35",
+    "cost": NaN,
+    "type": "Armor"
+  },
+  {
+    "name": "9",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "35",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "60",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "20",
+    "cost": NaN,
+    "type": "Melee Weapons"
+  },
+  {
+    "name": "30",
+    "cost": NaN,
+    "type": "Melee Weapons"
+  },
+  {
+    "name": "50",
+    "cost": NaN,
+    "type": "Melee Weapons"
+  },
+  {
+    "name": "50",
+    "cost": NaN,
+    "type": "Armor"
+  },
+  {
+    "name": "45",
+    "cost": NaN,
+    "type": "Melee Weapons"
+  },
+  {
+    "name": "45",
+    "cost": NaN,
+    "type": "Armor"
+  },
+  {
+    "name": "8",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "25",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "12",
+    "cost": NaN,
+    "type": "Ranged Weapons"
+  },
+  {
+    "name": "8",
+    "cost": NaN,
+    "type": "Melee Weapons"
+  },
+  {
+    "name": "15",
+    "cost": NaN,
+    "type": "Goetic Powers"
+  },
+  {
+    "name": "10",
+    "cost": NaN,
+    "type": "Goetic Powers"
+  },
+  {
+    "name": "20",
+    "cost": NaN,
+    "type": "Goetic Powers"
+  },
+  {
+    "name": "5",
+    "cost": NaN,
+    "type": "Goetic Powers"
+  },
+  {
+    "name": "2",
+    "cost": NaN,
+    "type": "Gear"
+  },
+  {
+    "name": "10",
+    "cost": NaN,
+    "type": "Gear"
+  },
+  {
+    "name": "15",
+    "cost": NaN,
+    "type": "Gear"
+  },
+  {
+    "name": "20",
+    "cost": NaN,
+    "type": "Gear"
+  },
+  {
+    "name": "3",
+    "cost": NaN,
+    "type": "Alchemy"
+  },
+  {
+    "name": "25",
+    "cost": NaN,
+    "type": "Alchemy"
+  },
+  {
+    "name": "15",
+    "cost": NaN,
+    "type": "Skills"
+  },
+  {
+    "name": "45",
+    "cost": NaN,
+    "type": "Skills"
+  },
+  {
+    "name": "10",
+    "cost": NaN,
+    "type": "Skills"
+  },
+  {
+    "name": "5",
+    "cost": NaN,
+    "type": "Skills"
+  },
+  {
+    "name": "20",
+    "cost": NaN,
+    "type": "Skills"
+  }
+]
